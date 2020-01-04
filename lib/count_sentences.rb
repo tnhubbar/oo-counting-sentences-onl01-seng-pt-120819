1a9a2fff60cd4abca812217d80b286c05cde0054
Taryn Johnson <taryn.johnson919@gmail.com>
2020-01-04 23:53:05 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,6 +15,11 @@ class String
     self.end_with? ("!")
   end
 
+
+#Think about the steps you need to go through to enact the desired behavior:
+
+#split the string on any and all periods, question marks and exclamation marks.
+#count the number of elements that results from that split.
   def count_sentences
     self.split(/\.|\?|\!/).delete_if {|w| w.size < 2}.size
   end
